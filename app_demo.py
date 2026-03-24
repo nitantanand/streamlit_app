9573b8c8cc10c3947b5c7d3251dc9c285a6d25d9
Nitant Anand <nitant.anand26@gmail.com>
2026-03-24 23:44:38 +0530

    Disclaimer add

--- app_demo.py
+++ app_demo.py
@@ -3,3 +3,4 @@ import streamlit as st
 #welcome page
 st.title("Hello this is an app demo")
 st.write("**Hi my name is Nitant**")
+st.write("This is a simple app demo to show how to use Streamlit to create a web app. I will be adding more features to this app in the future. Stay tuned!")
