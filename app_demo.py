49beadd6c758a9eb01ed8d93d40c169aafb35ee0
Nitant Anand <nitant.anand26@gmail.com>
2026-03-25 13:29:47 +0530

    image attached new

--- app_demo.py
+++ app_demo.py
@@ -4,4 +4,4 @@ import streamlit as st
 st.title("Hello this is an app demo")
 st.write("**Hi my name is Nitant**")
 st.write("This is a simple app demo to show how to use Streamlit to create a web app. I will be adding more features to this app in the future. Stay tuned!")
-st.image("https://www.freepik.com/vectors/data-engineering", caption="Online Image")
+st.image("https://montecarlodata.com/wp-content/uploads/2023/08/basic-steps-for-data-engineering-workflow.webp", caption="Online Image")
